d6bfd55b4709371f27790558fe3bdf168df78c98
NameNull <945351749@qq.com>
2015-12-08 11:32:39 +0800

    修改首页前提交

---
+++ public/runtime/data/helps_auto_cache/c7/c6/~@645d8af9fa86f8f4fdfc09dd76fc774d.php
@@ -0,0 +1,310 @@
+<?php
+//000000086400a:6:{i:0;a:7:{s:2:"id";s:1:"1";s:5:"title";s:12:"用户协议";s:7:"content";s:18088:"<div class="layout960">
+	<p>
+		<br />
+	</p>
+	<p class="MsoNormal" align="center" style="text-align:center;background:white;">
+		用户协议<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		众创园（网址<span>:</span><span><a href="http://www.zcfunding.com/">http://www.zcfunding.com</a></span>）是深圳前海招股金融服务有限公司旗下经营的网站。本协议双方为深圳前海招股金融服务有限公司（简称“众创园”）与众创园项目发起人（简称“您”或“用户”）。在您使用众创园提供的服务之前，请务必认真阅读并理解本协议的全部内容。本协议一经您点击确认并同意接受，即在您与众创园之间产生法律效力。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		一、 定义<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、项目发起人：在众创园平台注册并发起众筹项目的单位或个人；<span><br />
+2</span>、项目投资人：在众创园平台注册并投资某众筹项目的单位或个人。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		3、服务费：项目发起人使用众创园平台服务，众筹成功后，众创园将向发起人收取募集总金额<span>5%</span>的平台服务费；若众筹失败，众创园不收取服务费。发起人与众创园另有约定的除外。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		4、资金支付渠道费：为保证众筹资金安全，众筹项目项下资金均通过第三方支付公司代为划转，项目众筹成功后，众创园将扣除项目众筹总金额的<a><span>1.5%</span></a>&nbsp;作为资金支付渠道费，并支付给第三方支付机构。若项目众筹失败，众筹资金将原路返还给投资人，项目发起人也不需要承担任何支付渠道费。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		5、保证金：项目发起人在众创园平台发起的项目，由众创园作为代收款方，收取项目投资人基于项目投资明细单而支付的众筹金额。项目众筹成功后，众创园扣除服务费和支付渠道费，留存剩余众筹总金额的<span>30%</span>作为保证金，在项目发起人兑现对项目投资人的回报承诺后，众创园将该保证金返还给项目发起人。如果项目发起人未兑现对项目投资人的回报承诺，则众创园有权直接支配该保证金以用于兑现对项目投资人的回报承诺。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		二、产品众筹交易流程
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、产品众筹流程：<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>1</span>）众筹申请：投资人在众创园发起众筹项目，等待众创园审核通过后上线。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>2</span>）众筹期间：投资人在众筹期间，可以支持自己中意的项目，并且获得特定的数字权益。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>3</span>）众筹结束<span>5</span>日内：众创园向项目发起人发放扣除服务费和支付渠道费后的<span>70%</span>的资金。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>4</span>）众筹结束<span>7</span>日内：该数字权益开启交易。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>5</span>）权益交割期：权益交割期为<span>7</span>天，交割期内，投资人需要主动将数字权益兑换为产品，逾期不予兑换的，系统将自动为其兑换。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>6</span>）交割期满发货：投资人确认收到货之后，众创园将向项目发起人发放扣除服务费和支付渠道费后的<span>30%</span>的资金尾款。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		2、众筹项目包括三个阶段：产品众筹阶段，数字权益交易阶段，回报交割阶段。众创园众筹交易详细流程如下：<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		项目发起人在众创园发起<span>A</span>产品的众筹项目，并规定一个<span>A</span>产品对应<span>N</span>个数字权益（该数字权益只针对<span>A</span>产品有效）。如果一共成功众筹<span>M</span>个<span>A</span>产品，则<span>A</span>产品数字权益总量为<span>M*N</span>。<span></span> 
+	</p>
+	<p class="MsoListParagraph" align="left" style="margin-left:18pt;text-indent:-18pt;background-color:white;">
+		1） <b>产品众筹阶段</b>：投资人在众创园选择项目并参与众筹，同时获得数字化的众筹权益。数字化众筹权益与众筹项目的产品相对应。如果用户参与众筹，并购买了<span>2</span>个<span>A</span>产品，则其获得<span>2*N</span>个<span>A</span>产品的数字权益。<span></span> 
+	</p>
+	<p class="MsoListParagraph" align="left" style="margin-left:18pt;text-indent:-18pt;background-color:white;">
+		2） <b>数字权益交易阶段</b>：投资人可以在交易市场买入或者卖出数字权益，实现众筹权益流动。数字权益交易的平台为众创园合作单位众股交易平台（<span>www.zhgtrade.com</span>）。持有<span>A</span>产品数字权益的用户可以在交易市场以一定价格卖出自己拥有的权益，同时其他用户可以在交易市场买入<span>A</span>产品数字权益。在交易市场，卖出<span>A</span>产品数字权益则表示放弃<span>A</span>产品众筹回报，而买入者则获得<span>A</span>产品众筹回报。 <span></span> 
+	</p>
+	<p class="MsoListParagraph" align="left" style="margin-left:18pt;text-indent:-18pt;background-color:white;">
+		3） <b>回报交割阶段</b>：拥有数字权益的用户可以凭借持有的众筹数字权益来获取相应数量的产品。在整个众筹交易过程中，众筹数字权益与产品对应关系不变。如果用户<span>1</span>在产品<span>A</span>交割期拥有<span>A</span>产品的数字权益数目为<span>X</span>个，则用户<span>1</span>可以从项目发起者处获得<span>[X/N]</span>个产品回报，<span>[]</span>为对<span>X</span>除以<span>N</span>取整数。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		3、在回报交割前两周，众创园通知投资人及时交易，将自己拥有数字权益调整为单个产品对应数字权益数量（即<span>N</span>）的整数倍，保证投资人获得回报时的利益。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		4、数字权益交易在众筹完成<span>7</span>天内开始，具体时间见众创园及众股交易平台公告。数字权益交易在回报交割日前<span>1</span>天终止交易。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		5、在众创园，每个众筹项目产品的数字权益生命周期为众筹发起至回报交割结束。回报交割结束后，该产品对应的数字权益将失效。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		三、数字权益交易规则
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、交易时间<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		全天<span>24</span>小时交易；<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		2、交易模式<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		按价格优先、时间优先的原则进行撮合交易，当日买入的权益可以当日卖出，即实行<span>T+0</span>制度，特殊处理的权益除外；<span>&nbsp;</span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		3、特殊处理<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>1</span>）对连续<span>5</span>个交易日内交易价均未达到初始发行价的数字权益进行特殊处理；<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>2</span>）对任何交易日内交易价格触及或低于初始发行价的<span>50%</span>的数字权益进行特殊处理；<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		（<span>3</span>）<span>&nbsp;</span>特殊处理的数字权益需要暂时停盘，特殊处理时间一般为<span>1-3</span>天，需要请项目发起人披露产品最新的市场动态并作特殊说明，特殊处理结束后可以放开限制，进入正常交易环节。此外，如情节严重，对特殊处理的数字权益可以实施特别停盘。<span>&nbsp;</span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		四、数字货币众筹说明
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、数字货币指的是比特币以及与比特币具有相似技术原理的加密数字货币的统称。数字货币被央行定义为虚拟商品，用户可以自由买卖。数字货币众筹是指项目发起人在众创园上发起的数字货币的分发众筹，属于数字货币的预售行为。 <span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		2、数字货币项目发起人禁止使用数字货币从事洗钱、走私、商业贿赂等一切非法交易活动以及触犯国家法律等行为，若发现违法事件，众创园将即刻下线其众筹，并立即报送公安机关，由此所产生的一切损失将由项目发起人自行承担。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		3、投资人投资数字货币项目之前，请仔细阅读央行等五部委《关于防范比特币风险的通知》，认清和评估自身风险，理性投资，并合理规避风险。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		4、数字货币项目众筹成功之后，众筹资金将不退回；若众筹失败，资金会原路返还给投资人，请理性投资。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		5、众创园只负责数字货币项目的初步审核与众筹，众筹成功后，众创园不对该项目后续的进展和规划做任何背书，也不承担任何责任。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		五、本协议的构成及效力<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、所有众创园已经发布的或将来发布的各类规则，都属于本协议不可分割的一部分，与本协议具有同等的法律效力。<span><br />
+2</span>、众创园有权根据需要适时修订本协议及各类规则，并在网站上公示。变更后的协议和规则一经在众创园站上公示，即发生法律效力。<span><br />
+3</span>、在使用众创园提供的服务之前，请务必认真阅读本协议的全部内容。如您对本协议有任何疑问，应向众创园咨询。本协议生效后，您不应以未阅读或不接受本协议的内容为由，主张本协议无效或要求撤销本协议。<span><br />
+4</span>、您应该按照本协议约定行使权利并履行义务。如您不能接受本协议的约定，包括但不限于不能接受修订后的协议及各类规则，则您应立即停止使用众创园针对项目发起人提供的服务。如您继续使用众创园针对项目发起人提供的服务，则表示您同意并接受本协议及各类规则的约束。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		六、项目发起人的资格<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、作为项目发起人，您应为众创园的注册用户，完全理解并接受本协议。<span><br />
+2</span>、作为项目发起人的单位应为依法成立并登记备案的企业法人或其他组织；作为项目发起人的个人，应为年满<span>18</span>周岁并具有完全民事行为能力和民事权利能力的自然人，如项目发起人未满<span>18</span>周岁，应由其监护人代为履行本协议权利和义务。<span><br />
+3</span>、您应按照众创园的要求，进行必要的身份认证和资质认证，包括但不限于身份证、护照、学历证明等的认证。<span><br />
+4</span>、您应拥有在中国大陆地区开户并接收人民币汇款的银行卡或支付宝账户。<span><br />
+5</span>、您应妥善保管在众创园的用户名和密码，凡使用您的用户名和密码登陆众创园进行的一切操作，均视为您本人的行为，一切责任由您本人承担。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		七、您发起的众筹项目应符合以下要求<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、您在众创园上发起的项目，应明确具体的开始时间和结束时间。截止项目结束时间，如项目众筹金额低于预定众筹金额，则项目众筹失败；如项目众筹金额等于或大于预定众筹金额，则项目众筹成功。<span><br />
+2</span>、您不应抄袭、盗用他人的成果发起众筹项目，创意类产品必须为原创，涉及到知识产权的项目请尽量申请产权保护，因众筹造成的创意泄露或产权纠纷，众创园不承担任何责任。<span><br />
+3</span>、您发起的项目不得包含非法、色情、淫秽、暴力等内容，不得含有攻击性、侮辱性言论，不得含有违反国家法律法规、政策的内容及其他众创园认为不适宜的内容。<span><br />
+4</span>、您在众创园上发起的项目不得涉及种族主义、宗教极端主义、恐怖主义等内容。<span><br />
+5</span>、您应对自己及您发起的项目进行介绍，内容包括“关于我”、“发起该项目的目的”、“项目的进展和风险”、“为什么以众筹的方式发起该项目”、“项目众筹成功后的回报”等。同时，您应向项目投资人充分说明项目存在的风险及挑战，以便于项目投资人对项目有全面充分的了解，从而独立慎重作出是否投资的决定。<span><br />
+6</span>、您发起的项目应内容完整、合理，具有可行性。<span><br />
+7</span>、您发起的项目不应与第三方存在任何权利纠纷，否则因此导致的一切损失（包括众创园因此被第三方权利追索而遭受的一切损失）由您本人承担，与众创园无关。<span><br />
+8</span>、在项目发起后的运行过程中，您应及时回复网友的提问，与项目投资人进行充分互动，促成项目众筹成功。<span><br />
+9</span>、项目发起后，您应及时更新项目进展情况，以包括但不限于以照片、视频、素描等方式展示项目的进度信息。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		八、项目回报<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、您在发起项目时，应明确项目众筹成功后的回报时间。<span><br />
+2</span>、您承诺，如项目众筹成功，及时兑现对项目投资人承诺的回报；如项目众筹失败，同意众创园将众筹款项及时退还项目投资人，并由您就项目众筹失败的原因等对项目投资人作出解释。<span><br />
+3</span>、如您与项目投资人在兑现回报过程中发生纠纷，一切责任由您本人承担，如因此给众创园造成经济或名誉损失，您应赔偿损失、恢复名誉。<span><br />
+4</span>、如您在兑现对项目投资人的回报过程中，与第三方（包括但不限于物流公司）发生纠纷，一切责任由您本人承担，与众创园无关。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		九、协议终止<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		1、以下任一情形出现，本协议即终止<span><br />
+&nbsp;</span>（<span>1</span>）您发起的项目违反了国家法律法规、政策或众创园的平台使用规则或本协议约定，众创园停止为您提供服务的；<span><br />
+&nbsp;</span>（<span>2</span>）您本人不同意接受本协议约定（含众创园发布的各类规则），并停止使用众创园针对项目发起人提供的服务的；<span><br />
+&nbsp;</span>（<span>3</span>）您不符合本协议约定的项目发起人应具备的资格的；<span><br />
+&nbsp;</span>（<span>4</span>）其他原因导致本协议终止的。<span><br />
+2</span>、本协议终止后，不影响您因在本协议终止前的行为应承担的义务和责任。<span></span> 
+	</p>
+	<p class="MsoNormal" align="left" style="background-color:white;">
+		十、本协议的解释权归深圳前海招股金融服务有限公司所有。<span></span> 
+	</p>
+	<p class="MsoNormal">
+		<br />
+	</p>
+	<p>
+		<br />
+	</p>
+</div>";s:4:"type";s:4:"term";s:3:"url";s:38:"/zcfunding/index.php?ctl=help&act=term";s:6:"is_fix";s:1:"1";s:4:"sort";s:1:"1";}i:1;a:7:{s:2:"id";s:1:"2";s:5:"title";s:12:"服务介绍";s:7:"content";s:3354:"<p style="margin-left:21.0pt;text-indent:-21.0pt;background:white;">
+	<b>一、</b><b>产品众筹<span></span></b>
+</p>
+<p style="margin-left:0cm;text-indent:21.0pt;background:white;">
+	我们希望你是——充满想象力、创意满满的智能硬件；或把设计做到极致，让外貌协会忍不住点赞的实用性电子产品；艺术家的个人创作项目；面向初级收藏的文玩艺术品、有创意的原创设计类众筹项目。在这里你将找到你的首批体验用户、代理商伙伴和潜在的投资人。而我们将为你提供项目包装、媒体宣传、供应链合作、渠道资源和融资等一系列的上下游服务，帮助实现你的科技创业梦。<span></span>
+</p>
+<p style="margin-left:0cm;background:white;">
+	<b>科技众筹支持的项目类型</b>智能硬件<span></span>
+</p>
+<p style="margin-left:0cm;background:white;">
+	科技新材料类<span><br />
+</span>基于科技产品的创新型外观设计<span></span>
+</p>
+<p style="margin-left:0cm;background:white;">
+	<b>暂不接受以下分类</b>医疗产品、保健产品类<span><br />
+</span>大型机械、工业设备类（大型水泵、污水处理设备、数控机床、自动化生产线等）<span><br />
+</span>移动支付相关产品（<span>POS</span>机、支付系统）<span><br />
+</span>目前科技无突破类（我方无辨别的能力，如电池技术等）<span><br />
+</span>偏重营销甚至传销类<span>APP</span>（如吸粉器、加盟推广等）<span></span>
+</p>
+<p style="margin-left:0cm;background:white;">
+	<b>需要提供的资质（扫描件且需盖章<span>/</span>签字）<span></span></b>
+</p>
+<p style="margin-left:0cm;background:white;">
+	1、个人：身份证正反面电子版<span><br />
+2</span>、企业：企业营业执照<span><br />
+</span>税务登记证法人身份证组织机构代码证技术及产品外观设计等专利信息<span></span>
+</p>
+<p style="margin-left:0cm;background:white;">
+	<b>项目要求<span></span></b>
+</p>
+<p style="margin-left:0cm;background:white;">
+	1、 不能在其他众筹平台同步众筹。<span><br />
+2</span>、 硬件产品提供样品，材料类提供技术参数及相关专利。<span><br />
+3</span>、 产品不得涉及专利侵权等法律问题。<span><br />
+4</span>、 文案完整且不得抄袭，文字逻辑清晰无错别字无语病，图片质量高，能完整介绍产品的外观和用途。<span><br />
+5</span>、 能提供有效的客服支持，解决用户在众筹平台上的提问，对产品的进程给予及时的反馈。<span></span>
+</p>
+<p style="margin-left:21.0pt;text-indent:-21.0pt;background:white;">
+	<b>二、</b><b>数字货币众筹<span></span></b>
+</p>
+<p style="margin-left:18.0pt;text-indent:-18.0pt;background:white;">
+	1、
+团队信息公开；<span></span>
+</p>
+<p style="margin-left:18.0pt;text-indent:-18.0pt;background:white;">
+	2、
+数字货币具备加密、去中心化、总量恒定等基本属性，属于与比特币同类的加密货币；<span></span>
+</p>
+<p style="margin-left:18.0pt;text-indent:-18.0pt;background:white;">
+	3、
+众创园平台只提供众筹分发平台，不对数字货币本身的价值和未来做任何预期和担保，平台保有绝对的独立第三性特性。<span></span>
+</p>";s:4:"type";s:5:"intro";s:3:"url";s:39:"/zcfunding/index.php?ctl=help&act=intro";s:6:"is_fix";s:1:"1";s:4:"sort";s:1:"1";}i:2;a:7:{s:2:"id";s:1:"3";s:5:"title";s:12:"隐私策略";s:7:"content";s:0:"";s:4:"type";s:7:"privacy";s:3:"url";s:41:"/zcfunding/index.php?ctl=help&act=privacy";s:6:"is_fix";s:1:"1";s:4:"sort";s:1:"1";}i:3;a:7:{s:2:"id";s:1:"4";s:5:"title";s:12:"关于我们";s:7:"content";s:8189:"<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;">
+	<b>一、公司简介</b> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:24pt;">
+	深圳前海招股金融服务有限公司，简称招股金服（<span class="apple-converted-space">&nbsp;</span>ZHAOGU FINANCIAL），主营基于流动数字权益的众筹交易业务。公司目前拥有4位合伙人，均毕业于清华大学电子工程系。公司旗下拥有基于流动数字权益的众筹服务平台众创园（www.zcfunding.com）（改版升级中）、数字权益交易平台众股交易网（www.zhgtrade.com）等产品线。7个月来，两大业务线已经累计服务于多家创业团队以及6万余名投资用户。众股交易网的目标是打造国内最具口碑的数字权益交易平台，而众创园则是国内首家基于流动数字权益的众筹服务平台。众创园首创的基于流动数字权益的产品众筹模式直击传统众筹行业的痛点，使得产品众筹的退出策略成为可能。
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;">
+	<b>二、团队简介</b> 
+</p>
+<div style="text-align:center;">
+	<img src="http://www.zcfunding.com/public/attachment/201511/08/00/563e2795ec82e.jpg" alt="" />
+</div>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;">
+	<b>1</b><b>、主要创始人</b> 
+</p>
+<p class="MsoNormal" style="margin-left:21pt;color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:-21pt;">
+	Ø&nbsp;&nbsp;<b>鄢 傲，招股金服合伙人、CEO</b> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	湖北天门人，2015年7月毕业于清华大学电子工程系，研究生学历。在物联网领域累计发表多篇国际论文；曾工作于Hitachi（China）和招商银行总行，在物联网和数字货币领域具有多年的实践经验。主要负责公司的整体运营管理、商务合作；并负责众筹项目甄选。
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	业内知名媒体“<b>比特帮</b>”专访：
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	<a href="http://www.btcside.com/new/detail/4007">http://www.btcside.com/new/detail/4007</a><u></u> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	主要经历：
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	（1）、2013年接触数字货币，并在清华大学专注于区块链技术的推广工作；
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	（2）、2013年10月凭借5篇物联网协议国际论文获得清华大学国家奖学金；
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	（3）、2015年3月，与清华好友一起合伙创立招股科技公司，同年10月成立招股金服公司；
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	（4）、2015年9月，推出了国内数字货币区块链领域的首个专利。
+</p>
+<p class="MsoNormal" style="margin-left:21pt;color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:-21pt;">
+	Ø&nbsp;&nbsp;<b>程 超，招股金服合伙人、CTO</b> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	河南新县人，2015年7月毕业于清华大学电子工程系，研究生学历。在混合网络及视频加密领域累计发表多篇国际论文；曾工作于网易游戏，并参与多项工程项目开发，具备丰富的研发经验。主要负责公司众股网、众创园等业务线的研发和业务合作，并主导招股Z-Watch等智能硬件软件系统开发。
+</p>
+<p class="MsoNormal" style="margin-left:21pt;color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:-21pt;">
+	Ø&nbsp;&nbsp;<b>向忠胜，招股金服合伙人、CMO</b> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	江苏扬州人，2015年7月毕业于清华大学电子工程系，研究生学历。在超大规模网络系统仿真领域累计发表了多篇国际论文；曾工作于Ericsson集团、意法半导体（中国）、华为控股公司，在网络营销和数字货币领域具有多年的实践经验。目前主要负责产品营销推广、新业务拓展、以及公司品牌维护。
+</p>
+<p class="MsoNormal" style="margin-left:21pt;color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:-21pt;">
+	Ø&nbsp;&nbsp;<b>张国勇，招股金服合伙人、CFO</b> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:21pt;">
+	河南光山人，2015年7月毕业于清华大学电子工程系，研究生学历。在操作系统和系统级SoC设计领域累计发表了多篇国际论文；曾工作于联想集团和招商银行总行，在数字货币领域具有多年的实践经验。主要负责公司财务管理以及人力资源工作，并协助CEO统筹众筹项目甄选。
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;">
+	<b>2</b><b>、核心外包团队</b> 
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:24pt;">
+	外包团队主要负责公司部分非核心业务的开发和维护工作，减缓公司人员和资金压力。主要外包团队来自于：
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:24pt;">
+	招银网络科技；
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:24pt;">
+	网易游戏（广州）；
+</p>
+<p class="MsoNormal" style="color:#333333;font-family:'Microsoft Yahei', 微软雅黑, Arial, 'Hiragino Sans GB', 宋体;font-size:15px;background-color:#FFFFFF;text-indent:24pt;">
+	华为等。
+</p>";s:4:"type";s:5:"about";s:3:"url";s:39:"/zcfunding/index.php?ctl=help&act=about";s:6:"is_fix";s:1:"1";s:4:"sort";s:1:"1";}i:4;a:7:{s:2:"id";s:1:"5";s:5:"title";s:12:"官方微博";s:7:"content";s:27:"官方微博：招股科技";s:4:"type";s:0:"";s:3:"url";s:28:"http://weibo.com/zhaogutech/";s:6:"is_fix";s:1:"0";s:4:"sort";s:1:"1";}i:5;a:7:{s:2:"id";s:1:"7";s:5:"title";s:12:"撰写指南";s:7:"content";s:0:"";s:4:"type";s:11:"write_guide";s:3:"url";s:52:"http://www.zcfunding.com/index.php?ctl=article&id=77";s:6:"is_fix";s:1:"1";s:4:"sort";s:1:"1";}}
+?>
---
+++ public/runtime/data/user_level_auto_cache/c0/c1/~@6406aa4b28b36a2f61e3522bd0e6b09e.php
@@ -0,0 +1,3 @@
+<?php
+//000000086400a:1:{i:0;a:1:{s:4:"icon";s:45:"./app/Tpl/fanwe_1/images/level_icon/level.png";}}
+?>
